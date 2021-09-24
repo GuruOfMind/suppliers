b8876be0377879f5471010efae461c67c158d213
Almkdad M. Ali <almkdad.m.ali@gmail.com>
2021-09-24 17:15:12 +0300

    working version

--- request.js
+++ request.js
@@ -1,16 +1,18 @@
 function suppliersData() {
     return {
         suppliers: [],
+        suppliersListLength: 0,
         loadSuppliers() {
             axios.get('http://127.0.0.1:8000/api/suppliers')
             .then(
                 (response) => {
                     this.suppliers = response.data;
+                    this.suppliersListLength = this.suppliers.length;
                 }
             )
             .catch(
                 (error) => {
-                console.log(error)
+                    console.log(error);
                 }
             );
         },
@@ -19,6 +21,8 @@ function suppliersData() {
             axios.post('http://127.0.0.1:8000/api/suppliers', formData, { headers: {'Content-Type': 'multipart/form-data'} } )
             .then(
                 (response) => {
+                    console.log(formData);
+                    this.$refs.supplierForm.reset();
                     this.loadSuppliers();
                 }
             )
@@ -34,6 +38,7 @@ function suppliersData() {
                     (response) => {
                         let removeIndex = this.suppliers.map(item => item.id).indexOf(supplierId);
                         ~removeIndex && this.suppliers.splice(removeIndex, 1);
+                        this.suppliersListLength--;
                     }
                 )
                 .catch(
@@ -43,8 +48,6 @@ function suppliersData() {
                 );
             },
         updateSupplier() {
-            // var formData = new FormData();
-            // var imagefile = document.querySelector('#file');
         },
     }
 }
